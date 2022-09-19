718ef5592b835d48373064ef5e6774d6fb88ddbb
Hyunggi <phg9587@naver.com>
2022-09-20 00:07:53 +0900

    2.3 Paint Color part One

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 
@@ -25,8 +26,12 @@ function cancelPainting() {
     ctx.beginPath();
 }
 function onLineWidthChange(event) {
-    console.log(event.target.value);
     ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -42,3 +47,6 @@ canvas.addEventListener("mouseleave", cancelPainting);
 
 
 lineWidth.addEventListener("change", onLineWidthChange);
+
+color.addEventListener("change", onColorChange);
+
