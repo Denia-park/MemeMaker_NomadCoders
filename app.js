d0f9e6e5cecff703615040687cdc4cbe4c54fdbc
Hyunggi <phg9587@naver.com>
2022-09-19 18:17:00 +0900

    1.1 Our First Drawing

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("Hello World");
+const canvas = document.querySelector('canvas');
+
+//context 를 ctx 로 짧게 명명
+const ctx = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.fillRect(50, 50, 100, 200);
